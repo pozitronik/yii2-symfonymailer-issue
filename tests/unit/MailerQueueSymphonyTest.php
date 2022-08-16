aaa7e48643d52948b50410042e91edfb58dc490d
antikillerxxl@gmail.com <Pozitronik>
2022-08-16 12:16:59 +0300

    Let's see, what happen after `unset($message->mailer);`

--- tests/unit/MailerQueueSymphonyTest.php
+++ tests/unit/MailerQueueSymphonyTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 use Codeception\Test\Unit;
 use yii\base\Exception;
 use yii\base\InvalidConfigException;
+use yii\symfonymailer\Message;
 
 /**
  * Class MailerQueueSymphonyTest
@@ -47,6 +48,41 @@ class MailerQueueSymphonyTest extends Unit {
 			'mailer' => Yii::$app->mailer,
 			'message' => $message
 		]))->execute(Yii::$app->queue_email);*/
+
+	}
+
+	/**
+	 * @return void
+	 * @throws Exception
+	 * @throws InvalidConfigException
+	 */
+	public function testMailerQueueSymphonyUnsetTransportObject():void {
+		Yii::$app->set('queue_email', [
+			'class' => yii\queue\file\Queue::class,
+			'path' => '@runtime/queues/email',
+			'ttr' => 3600
+		]);
+		Yii::$app->set('mailer', [
+			'class' => YarCode\Yii2\QueueMailer\Mailer::class,
+			'queue' => 'queue_email',
+			'syncMailer' => [
+				'class' => yii\symfonymailer\Mailer::class,
+				'transport' => [
+					'scheme' => 'smtp',
+					'host' => 'localhost',
+					'port' => 25
+				],
+			],
+		]);
+
+		/** @var Message $message */
+		$message = Yii::$app->mailer->compose()
+			->setSubject('test email')
+			->setFrom('test@email.local')
+			->setTextBody(Yii::$app->security->generateRandomString())
+			->setTo('test@queueemail.local');
+		unset($message->mailer);
+		$this::assertTrue(Yii::$app->mailer->send($message));
 
 	}
 }
